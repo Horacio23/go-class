b323db493ab71c0ac891d4d2e82dc9c512492489
horacio23 <horacio.delgado23@gmail.com>
2016-05-01 23:25:16 -0400

    updated the iota example

--- aota/aota.go
+++ aota/aota.go
@@ -10,6 +10,14 @@ const (
 	c
 )
 
+// iota gets reset per constant block, meaning it has a non global scope
+const (
+	d = iota
+	e
+	f
+)
+
 func main() {
 	fmt.Println(a, b, c)
+	fmt.Println(d, e, f)
 }
