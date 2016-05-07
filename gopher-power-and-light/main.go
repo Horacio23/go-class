cd17625994f648dd91593eb61bd8d6d6e6f17513
Horacio Delgado <horacio.delgado23@gmail.com>
2016-05-07 16:41:38 -0400

    devided the code using functions

--- gopher-power-and-light/main.go
+++ gopher-power-and-light/main.go
@@ -7,9 +7,7 @@ func main() {
 	activePlants := []int{0, 1}
 	gridLoad := 75.
 
-	fmt.Println("1) Generate Power Plant")
-	fmt.Println("2) Generate Grid Report")
-	fmt.Println("Please secelt an option")
+	displayMenu()
 
 	var option string
 
@@ -17,18 +15,32 @@ func main() {
 
 	switch option {
 	case "1":
-		for idx, cap := range plantCapacities {
-			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
-		}
+		generatePlantCapacityReport(plantCapacities...)
 	case "2":
-		capacity := 0.
-		for _, plantId := range activePlants {
-			capacity += plantCapacities[plantId]
-		}
-		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
-		fmt.Printf("%-20s%.0f\n", "Load ", gridLoad)
-		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+		generatePowerGridReport(activePlants, plantCapacities, gridLoad)
 	default:
 		fmt.Println("Invalid input, please try again")
 	}
+}
+
+func generatePlantCapacityReport(plantCapacities ...float64) {
+	for idx, cap := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+	}
+}
+
+func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	capacity := 0.
+	for _, plantId := range activePlants {
+		capacity += plantCapacities[plantId]
+	}
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+}
+
+func displayMenu() {
+	fmt.Println("1) Generate Power Plant")
+	fmt.Println("2) Generate Grid Report")
+	fmt.Println("Please secelt an option")
 }
