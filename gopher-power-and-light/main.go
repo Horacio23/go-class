e1d0c9f157d26dababe3e6dbb2a8ed9b923c57bf
Horacio Delgado <horacio.delgado23@gmail.com>
2016-05-08 16:38:04 -0400

    updated the gopher app to use struts and methods

--- gopher-power-and-light/main.go
+++ gopher-power-and-light/main.go
@@ -1,25 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 func main() {
-	plantCapacities := []float64{30, 30, 30, 60, 60, 11}
-	activePlants := []int{0, 1}
-	gridLoad := 75.
+	plants := []PowerPlant{
+		PowerPlant{hydro, 300, active},
+		PowerPlant{wind, 30, active},
+		PowerPlant{wind, 25, inactive},
+		PowerPlant{wind, 35, active},
+		PowerPlant{solar, 45, unavailable},
+		PowerPlant{solar, 40, inactive},
+	}
+	grid := PowerGrid{300, plants}
 
-	displayMenu()
-
-	var option string
-
-	fmt.Scanln(&option)
-
-	switch option {
-	case "1":
-		generatePlantCapacityReport(plantCapacities...)
-	case "2":
-		generatePowerGridReport(activePlants, plantCapacities, gridLoad)
-	default:
-		fmt.Println("Invalid input, please try again")
+	if option, err := displayMenu(); err == nil {
+		switch option {
+		case "1":
+			grid.generatePlantReport()
+		case "2":
+			//TODO
+		default:
+			fmt.Println("Invalid input, please try again")
+		}
 	}
 }
 
@@ -39,8 +45,54 @@ func generatePowerGridReport(activePlants []int, plantCapacities []float64, grid
 	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
 }
 
-func displayMenu() {
+func displayMenu() (option string, err error) {
 	fmt.Println("1) Generate Power Plant")
 	fmt.Println("2) Generate Grid Report")
 	fmt.Println("Please secelt an option")
+
+	fmt.Scanln(&option)
+
+	if option != "1" && option != "2" {
+		err = errors.New("Invalid option selected")
+	}
+	return
+}
+
+type PlantType string
+
+const (
+	hydro PlantType = "Hydro"
+	wind  PlantType = "Wind"
+	solar PlantType = "Solar"
+)
+
+type PlantStatus string
+
+const (
+	active      PlantStatus = "Active"
+	inactive    PlantStatus = "Inactive"
+	unavailable PlantStatus = "Unavailable"
+)
+
+type PowerPlant struct {
+	plantType PlantType
+	capacity  float64
+	status    PlantStatus
+}
+
+type PowerGrid struct {
+	load   float64
+	plants []PowerPlant
+}
+
+func (pg *PowerGrid) generatePlantReport() {
+	for idx, p := range pg.plants {
+		label := fmt.Sprintf("%s%d", "Plant #", idx)
+		fmt.Println(label)
+		fmt.Println(strings.Repeat("-", len(label)))
+		fmt.Printf("%-20s%s\n", "Type: ", p.plantType)
+		fmt.Printf("%-20s%.0f\n", "Capacity ", p.capacity)
+		fmt.Printf("%-20s%s\n", "Status: ", p.status)
+		fmt.Println("")
+	}
 }
