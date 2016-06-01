20ddf8c41f0f6025bc71f450074b4c37050afff0
Horacio Delgado <horacio.delgado23@gmail.com>
2016-06-01 16:11:47 -0400

    added the default choice in case that the channels have no info to avoid a deadlock

--- concurrency/channels/switching.go
+++ concurrency/channels/switching.go
@@ -6,19 +6,19 @@ func main() {
 	msgCh := make(chan Message, 1)
 	errCh := make(chan FailedMessage, 1)
 
-	msg := Message{
-		To:      []string{"frodo@something.com"},
-		From:    "gandalf@toogreat.com",
-		Content: "Nerding out",
-	}
-
-	failedMessage := FailedMessage{
-		ErrorMessage:    "Message intercepted by black rider",
-		OriginalMessage: Message{},
-	}
-
-	msgCh <- msg
-	errCh <- failedMessage
+	// msg := Message{
+	// 	To:      []string{"frodo@something.com"},
+	// 	From:    "gandalf@toogreat.com",
+	// 	Content: "Nerding out",
+	// }
+	//
+	// failedMessage := FailedMessage{
+	// 	ErrorMessage:    "Message intercepted by black rider",
+	// 	OriginalMessage: Message{},
+	// }
+	//
+	// msgCh <- msg
+	// errCh <- failedMessage
 
 	// The first message to be true will be executed
 	// so the channels with higher priority should be eveluated first
@@ -27,6 +27,8 @@ func main() {
 		fmt.Println(receivedMsg)
 	case receivedError := <-errCh:
 		fmt.Println(receivedError)
+	default:
+		fmt.Println("Nothing to do here")
 	}
 
 }
