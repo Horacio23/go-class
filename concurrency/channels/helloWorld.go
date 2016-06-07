e2a2fc53b013c326e7aba670f20eea5c083441df
Horacio Delgado <horacio.delgado23@gmail.com>
2016-06-07 13:06:00 -0400

    created a mutex example

--- concurrency/channels/helloWorld.go
+++ concurrency/channels/helloWorld.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan string)
+	ch := make(chan string, 1) //hast to be buffered or it would block when putting in hello
 	ch <- "Hello"
 	fmt.Println(<-ch)
 }
